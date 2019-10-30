ea527378f9c6084db236c915a546caead3efced4
mmman64 <mcnultymarco@gmail.com>
2019-10-30 14:50:07 +0000

    📦 Add: New and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,28 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolio_items = Portfolio.all
+  end
+
+  def new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: "Your portfolio item is now live!" }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
   end
 end
