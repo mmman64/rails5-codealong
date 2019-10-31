97153d153c0328a4879b4f044031c6143b28abaa
mmman64 <mcnultymarco@gmail.com>
2019-10-31 17:39:33 +0000

    📦 NEW: Image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,11 @@
 class Portfolio < ApplicationRecord
 
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://place-hold.it/600x400"
+    self.thumb_image ||= "https://place-hold.it/350x200"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+
 10.times do |blog|
   Blog.create!(
     title: "My blog post #{blog}",
     body: "Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Donec id elit non mi porta gravida at eget metus. Nullam quis risus eget urna mollis ornare vel eu leo. Aenean lacinia bibendum nulla sed consectetur.
-    Aenean lacinia bibendum nulla sed consectetur. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Donec id elit non mi porta gravida at eget metus."
+    Aenean lacinia bibendum nulla sed consectetur. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Donec id elit non mi porta gravida at eget metus.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -18,10 +26,20 @@ end
 puts "5 skills created"
 
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body: "Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis.",
+    main_image: "https://place-hold.it/600x400",
+    thumb_image: "https://place-hold.it/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis.",
     main_image: "https://place-hold.it/600x400",
     thumb_image: "https://place-hold.it/350x200"
