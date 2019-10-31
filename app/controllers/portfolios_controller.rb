deab75c964b8a3a11868a97f3f853dcfb78de080
mmman64 <mcnultymarco@gmail.com>
2019-10-31 20:42:35 +0000

    📦 NEW: Nested attributes for portfolio technologies

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,8 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    # builds 3 technologies for the portfolio item
+    3.times { @portfolio_item.technologies.build }
   end
 
   def show
@@ -51,6 +53,6 @@ class PortfoliosController < ApplicationController
   end
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
+    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 end
