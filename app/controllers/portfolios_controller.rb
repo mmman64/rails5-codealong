2c516ecdcb2befb5fe40118cc6d2107b0329419f
mmman64 <mcnultymarco@gmail.com>
2019-11-08 18:02:58 +0000

    📦 NEW: Persistent position updates for portfolio drag & drop

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,14 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.by_position
   end
 
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+
+    head :ok
+  end
+
   def new
     @portfolio_item = Portfolio.new
     # builds 3 technologies for the portfolio item
