f77c4e361d0fe40fb66d0bc3d56e4c307e969175
mmman64 <mcnultymarco@gmail.com>
2019-11-11 22:05:32 +0000

    👌 IMPROVE: File upload forms and placeholder functionality in view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,8 +4,7 @@ class Portfolio < ApplicationRecord
                                 allow_destroy: true,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -13,13 +12,4 @@ class Portfolio < ApplicationRecord
   def self.by_position
     order("position ASC")
   end
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
-    self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
-  end
-
-
 end
